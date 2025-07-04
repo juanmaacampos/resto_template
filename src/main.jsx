649ab953dcfba042047e446d883342f9e3391b2c
Juan Manuel <juanmanuelccampos@gmail.com>
2025-07-04 14:49:18 -0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './Router.jsx'
 import { MenuProvider } from './context/MenuContext.jsx'
+import './styles/index.css';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+// Cambiar fondo a blanco por defecto para evitar fondo negro
+document.body.style.backgroundColor = '#fff';
+
+createRoot(rootElement).render(
   <StrictMode>
     <MenuProvider>
       <Router />
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://juanmaacampos.github.io/shop_template'
+  base: 'https://juanmaacampos.github.io/resto_template/'
 })
